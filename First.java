b513775704f4621c51c5ac7c07e3a907c127e4f2
KoryunY <81442008+KoryunY@users.noreply.github.com>
2021-03-27 00:08:31 +0400

    Add files via upload

---
+++ First.java
@@ -0,0 +1,20 @@
+public class First
+{
+    public static void main(String[] args)
+    {
+        System.out.println("...here we go again!");
+        byte a=21;
+        short s=412;
+        int d=2147483646;
+        long f=10141200L;
+        float g=4.1f;
+        double h = 123.4;
+        boolean j=false;
+        char k='.';
+        String l=";'";
+        System.out.println(a+s+d+f);
+        System.out.println(g);
+        System.out.println(h+"..."+j+k+l);
+    }
+}
+
